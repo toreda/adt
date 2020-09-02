14c694be180b64228b92cea40ee0ddc240700f64
SaxanLosterToreda <70411010+SaxanLosterToreda@users.noreply.github.com>
2020-09-02 15:53:12 -0700

    added pop and front

--- src/priority-queue.ts
+++ src/priority-queue.ts
@@ -49,4 +49,69 @@ export class ArmorPriorityQueue<T> implements ArmorCollection<T> {
 
 		return this;
 	}
+
+	public front(): T | null {
+		return this.elements[0];
+	}
+
+	public pop(): T | null {
+		if (this.size() == 0) {
+			return null;
+		}
+		else if (this.size() == 1) {
+			return this.elements.shift();
+		}
+		else {
+			let highestPriority = this.front();
+
+			if (this.size() === 3) {
+				highestPriority = this.elements.shift();
+				if (this.elements[0] > this.elements[1]){
+					let lastValue = this.elements[1];
+					this.elements[1] = this.elements[0];
+					this.elements[0] = lastValue;
+				}
+				return highestPriority;
+			}
+
+			let nodeIndex = 0;
+			let nodeValue = this.front();
+			let lastIndex = this.size() - 1;
+			let lastValue = this.elements.pop();
+
+			this.elements[nodeIndex] = lastValue;
+
+			nodeValue = lastValue;
+			let childLeftIndex = nodeIndex * 2;
+			let childLeftValue = this.elements[childLeftIndex] || null;
+			let childRghtIndex = nodeIndex * 2 + 1;
+			let childRghtValue = this.elements[childRghtIndex] || null;
+
+			while (childLeftValue && childRghtValue &&
+						(childLeftValue < nodeValue || childRghtValue < nodeValue)
+						) {
+				if (childLeftValue < childRghtValue) {
+					this.elements[nodeIndex] = childLeftValue;
+					this.elements[childLeftIndex] = nodeValue;
+					nodeIndex = childLeftIndex;
+				}
+				else {
+					this.elements[nodeIndex] = childRghtValue;
+					this.elements[childRghtIndex] = nodeValue;
+					nodeIndex = childRghtIndex;
+				}
+				childLeftIndex = nodeIndex * 2;
+				childRghtIndex = nodeIndex * 2 + 1;
+				childLeftValue = this.elements[childLeftIndex] || null;
+				childRghtValue = this.elements[childRghtIndex] || null;
+			}
+
+			if (childRghtValue === null && childLeftValue < nodeValue) {
+				this.elements[nodeIndex] = childLeftValue;
+				this.elements[childLeftIndex] = nodeValue;
+			}
+
+			return highestPriority;
+		}
+	}
 }
--- tests/priority-queue.spec.ts
+++ tests/priority-queue.spec.ts
@@ -12,18 +12,46 @@ describe('ArmorPriorityQueue', () => {
 	});
 
 	describe('Constructor', () => {
-		it('should initialize empty queue', () => {
+		it('should initialize empty priority queue', () => {
 			const custom = new ArmorPriorityQueue<number>();
 			expect(custom.size()).toBe(0);
 		});
 		
-		it('should initialize priority queue with arguments', () => {
-			const contents = [84513, 75648, 89745];
+		it('should initialize priority queue with argument', () => {
+			const contents = [84513, 75648, 89745, 24567, 95425];
+			const expected = [24567, 75648, 89745, 84513, 95425];
 			const custom = new ArmorPriorityQueue<number>(contents);
+
+			for (let i = 0; i < contents.length; i++ ){
+				const result = custom.pop();
+				expect(result).toBe(expected[i]);
+			}
+		});
+
+		it('should initialize empty priority queue when argument is not an array', () => {
+			const custom = new ArmorPriorityQueue<number>(44091 as any);
+			expect(custom.size()).toBe(0);
 		});
 	});
 
+	describe('push', () => {
+		it('should add exactly one item to priority queue when push is called once', () => {
+			expect(instance.size()).toBe(0);
+			instance.push(45943);
+			expect(instance.size()).toBe(1);
+		});
+		
+		it('should add exactly 15 items to priority queue when push is called 15 times', () => {
+			expect(instance.size()).toBe(0);
 
+			const limit = 15;
+			for (let i = 0; i < limit; i++ ) {
+				instance.push(Math.floor(Math.random() * 99999));
+			}
+
+			expect(instance.size()).toBe(limit);
+		});
+	});
 
 
 
