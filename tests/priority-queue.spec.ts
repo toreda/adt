c762ff7f984d6175d7623a044d432c12a4fc0784
SaxanLosterToreda <70411010+SaxanLosterToreda@users.noreply.github.com>
2020-09-03 16:28:40 -0700

    added some test for the helper functions

--- tests/priority-queue.spec.ts
+++ tests/priority-queue.spec.ts
@@ -1,4 +1,4 @@
-import {ArmorPriorityQueue} from '../src/priority-queue';
+import { ArmorPriorityQueue } from '../src/priority-queue';
 
 describe('ArmorPriorityQueue', () => {
 	let instance: ArmorPriorityQueue<number>;
@@ -11,7 +11,7 @@ describe('ArmorPriorityQueue', () => {
 		instance.clear();
 	});
 
-	describe('Constructor', () => {
+	describe('constructor', () => {
 		it('should initialize empty priority queue', () => {
 			const custom = new ArmorPriorityQueue<number>();
 			expect(custom.size()).toBe(0);
@@ -23,18 +23,20 @@ describe('ArmorPriorityQueue', () => {
 				{rank: 3, data: 75648},
 				{rank: 2, data: 89745},
 				{rank: 5, data: 24567},
-				{rank: 4, data: 95425},
+				{rank: 4, data: 95425}
 			];
 			const expected = [
 				{rank: 1, data: 84513},
 				{rank: 2, data: 89745},
 				{rank: 3, data: 75648},
 				{rank: 4, data: 95425},
-				{rank: 5, data: 24567},
+				{rank: 5, data: 24567}
 			];
-			const expected1 = contents.slice().sort((a, b) => {return a.rank < b.rank ? -1 : +1});
+			const expected1 = contents.slice().sort((a, b) => {
+				return a.rank < b.rank ? -1 : +1;
+			});
 			const custom = new ArmorPriorityQueue<number>(contents);
-			
+
 			for (let i = 0; i < contents.length; i++) {
 				const result = custom.pop();
 				expect(result).not.toBeNull();
@@ -46,6 +48,106 @@ describe('ArmorPriorityQueue', () => {
 			const custom = new ArmorPriorityQueue<number>(44091 as any);
 			expect(custom.size()).toBe(0);
 		});
+	});
+
+	describe('size', () => {
+		it('should return 0 when priority queue is empty', () => {
+			expect(instance.size()).toBe(0);
+		});
+
+		it('should return 0 if elements are missing', () => {
+			delete instance.elements;
+			expect(instance.size()).toBe(0);
+		});
+
+		it('should return the number of items in priority queue', () => {
+			const limit = 5;
+
+			for (let i = 0; i < limit; i++) {
+				instance.push({rank: Math.floor(Math.random() * 999), data: Math.floor(Math.random() * 99999)});
+			}
+
+			expect(instance.size()).toBe(limit);
+		});
+	});
+
+	describe('front', () => {
+		it('should return null when priority queue size is 0', () => {
+			expect(instance.size()).toBe(0);
+			expect(instance.front()).toBeNull();
+		});
+
+		it('should return the item with the lowest rank in priority queue', () => {
+			const items = [
+				{rank: 2, data: 95135},
+				{rank: 1, data: 75315},
+				{rank: 3, data: 45682}
+			];
+			items.forEach((item) => {
+				instance.push(item);
+			});
+			expect(instance.front()).toStrictEqual(items[1]);
+		});
+	});
+
+	describe('swapNodes', () => {
+		let unchanged: ArmorPriorityQueue<number>;
+
+		beforeAll(() => {
+			unchanged = new ArmorPriorityQueue<number>();
+		});
+
+		beforeEach(() => {
+			unchanged.clear();
+		})
+
+		const params = [
+			{name: 'null', value: null},
+			{name: 'below zero', value: -1},
+			{name: 'past end', value: 999},
+			{name: 'in array', value: 0},
+		];
+		const items = [
+			{rank: 2, data: 95135},
+			{rank: 1, data: 75315},
+			{rank: 3, data: 45682}
+		];
+
+		params.forEach(param1 => {
+			params.forEach(param2 => {
+				it(`should do nothing if indexOne is ${param1.name} and indexTwo is ${param2.name}`, () => {
+					items.forEach(item => {
+						instance.push(item);
+						unchanged.push(item);
+					});
+					instance.swapNodes(param1.value, param2.value)
+					expect(instance.elements).toStrictEqual(unchanged.elements);
+				});
+			});
+		});
+
+		it('should swap the order of the items from 1,2,3 to 2,1,3 to 2,3,1', () => {
+			items.forEach(item => {
+				instance.push(item);
+			});
+			instance.swapNodes(0, 1);
+			expect(instance.elements.map(v => v.rank)).toStrictEqual([2, 1, 3]);
+			instance.swapNodes(2, 1);
+			expect(instance.elements.map(v => v.rank)).toStrictEqual([2, 3, 1]);
+		});
+
+	});
+
+	describe('getParentNodeIndex', () => {
+
+	});
+
+	describe('getChildNodesIndexes', () => {
+
+	});
+
+	describe('getRankFromIndex', () => {
+
 	});
 
 	describe('push', () => {
@@ -130,41 +232,6 @@ describe('ArmorPriorityQueue', () => {
 			for (let i = 0; i < 5; i++) {
 				expect(instance.pop()).toBeNull();
 			}
-		});
-	});
-
-	describe('front', () => {
-		it('should return null when priority queue is empty', () => {
-			expect(instance.size()).toBe(0);
-			expect(instance.front()).toBeNull();
-		});
-
-		it('should return the item with the lowest rank in priority queue', () => {
-			const items = [
-				{rank: 2, data: 95135},
-				{rank: 1, data: 75315},
-				{rank: 3, data: 45682}
-			];
-			items.forEach((item) => {
-				instance.push(item);
-			});
-			expect(instance.front()).toStrictEqual(items[1]);
-		});
-	});
-
-	describe('size', () => {
-		it('should return 0 when priority queue is empty', () => {
-			expect(instance.size()).toBe(0);
-		});
-
-		it('should return the number of items in priority queue', () => {
-			const limit = 5;
-
-			for (let i = 0; i < limit; i++) {
-				instance.push({rank: Math.floor(Math.random() * 999), data: Math.floor(Math.random() * 99999)});
-			}
-
-			expect(instance.size()).toBe(limit);
 		});
 	});
 
